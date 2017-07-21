3eb046c9b57eb6a6e70a73930ff275df0ab4e34a
Guillaume GENCE <guillaume.gence@gmail.com>
2017-07-21 15:40:27 +0200

    Fix for linter

--- js/src/recTree.js
+++ js/src/recTree.js
@@ -6,15 +6,15 @@
  * @Last modified time: 2017-07-05T16:30:30+02:00
  */
 
-recTreeVisu = {};
+var recTreeVisu = {};
 
-recTreeVisu.vizualize = function (recPhyloXML,domContainerId) {
+recTreeVisu.vizualize = function (recPhyloXML, domContainerId) {
   var rootsClades;
   var recTrees;
-  //Parse le recPhyloXML, créer les branches mortes et stock dans un objet de type recTree
+  // Parse le recPhyloXML, créer les branches mortes et stock dans un objet de type recTree
   rootsClades = recTreeVisu.process(recPhyloXML);
-  //Calcul la position des elements de recTree (noueds, branches) dans un repère en 2 dimensions
+  // Calcul la position des elements de recTree (noueds, branches) dans un repère en 2 dimensions
   recTrees = recTreeVisu.map(rootsClades);
   // //Genere le svg dans le container domContainerId
-  recTreeVisu.render(recTrees,domContainerId);
-}
+  recTreeVisu.render(recTrees, domContainerId);
+};
